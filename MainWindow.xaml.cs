daf504e6a394b7ee020300e707b49778c795ff7d
Ibrahim Ali <47118191+ibrahiimmohd@users.noreply.github.com>
2020-10-12 15:39:39 -0400

    WIP: Added comboboxes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,9 +24,66 @@ namespace Assignment2
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<string> list = new ObservableCollection<string>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            foreach (RestaurantItem item in RestaurantItem.GetItems())
+            {
+                if (item.Category == "Beverage")
+                {
+                    comboBoxBeverage.Items.Add(item.Name);
+                }
+                else if (item.Category == "Appetizer")
+                {
+                    comboBoxAppetizer.Items.Add(item.Name);
+                }
+                else if (item.Category == "Main Course")
+                {
+                    comboBoxMainCourse.Items.Add(item.Name);
+                }
+                else if (item.Category == "Dessert")
+                {
+                    comboBoxDessert.Items.Add(item.Name);
+                }
+            }
+
+            //comboBoxBeverage.SelectedIndex = 0;
+            //comboBoxAppetizer.SelectedIndex = 0;
+            //comboBoxMainCourse.SelectedIndex = 0;
+            //comboBoxDessert.SelectedIndex = 0;
+
         }
+
+        private void ComboBoxBeverage_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //var selectedValue = comboBoxBeverage.SelectedValuePath;  //((ComboBoxItem)comboBoxBeverage.SelectedItem).Content.ToString();
+            //list.Add(selectedValue);
+            //dataGrid.ItemsSource = selectedValue;
+        }
+
+        private void ComboBoxMainCourse_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //var selectedValue = ((ComboBoxItem)comboBoxMainCourse.SelectedItem).Content.ToString();
+            //list.Add(selectedValue);
+            //dataGrid.ItemsSource = list;
+        }
+
+        private void ComboBoxDessert_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+           //var selectedValue = ((ComboBoxItem)comboBoxDessert.SelectedItem).Content.ToString();
+            //list.Add(selectedValue);
+            //dataGrid.ItemsSource = list;
+        }
+
+        private void ComboBoxAppetizer_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //var selectedValue = ((ComboBoxItem)comboBoxAppetizer.SelectedItem).Content.ToString();
+            //list.Add(selectedValue);
+            //dataGrid.ItemsSource = list;
+        }
+
     }
 }
